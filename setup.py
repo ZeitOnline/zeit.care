45331eacf387783619d778b53c4c8e8107833a2a
hoshi <devnull@localhost>
2009-03-26 12:27:51 +0000

    Package boilerplate...

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeit.care',
+    version='0.1dev',
+    author='Christian Zagrodnick, Ron Drongowski, Dominik Hoppe',
+    author_email='cz@gocept.com',
+    url='http://trac.gocept.com/zeit',
+    description="""\
+""",
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe=False,
+    license='gocept proprietary',
+    namespace_packages = ['zeit'],
+    install_requires=[
+        'zeit.connector',
+        'setuptools',
+        ],
+)
