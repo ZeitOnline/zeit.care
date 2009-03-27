f9cdef2cdc939c02cd65ecf1f67958853113574b
ron <devnull@localhost>
2009-03-27 12:59:15 +0000

    defined isofy_worker extended test for isofy_worker

--- src/zeit/care/worker.py
+++ src/zeit/care/worker.py
@@ -1,7 +1,22 @@
 import datetime
 import pytz
 
+def isofy_worker(resource, connector):
+    dlm = resource.properties.get((
+         'date-last-modified',
+         'http://namespaces.zeit.de/CMS/document'))
+    if dlm is not None:
+        dt = isofy_date_last_modified(dlm)
+        if dt is not None:
+            connector.changeProperties(resource.id, {(
+                'date-last-modified',
+                'http://namespaces.zeit.de/CMS/document'): dt})
+
+
 def isofy_date_last_modified(date):
-    dt = datetime.datetime.strptime(date, "%d.%m.%Y - %H:%M")
-    dt = dt.replace(tzinfo=pytz.timezone("Europe/Berlin")).astimezone(pytz.UTC)
+    try:
+        dt = datetime.datetime.strptime(date, "%d.%m.%Y - %H:%M")
+    except ValueError:
+        return None
+    dt = pytz.timezone("Europe/Berlin").localize(dt).astimezone(pytz.UTC)
     return dt.isoformat()
